829cfc6368fef6d2cc2c41be40b7bd65ba68e655
michinoins <>
2022-01-04 18:29:55 +0900

    first commit

---
+++ src/test/kotlin/com/event_app_backend/event_app_backend/EventAppBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.event_app_backend.event_app_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventAppBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
