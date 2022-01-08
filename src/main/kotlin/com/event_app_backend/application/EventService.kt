8069d37241378ed4222ec97a28df17615a3202cb
michinoins <>
2022-01-08 09:36:44 +0900

    add: view Events

--- src/main/kotlin/com/event_app_backend/application/EventService.kt
+++ src/main/kotlin/com/event_app_backend/application/EventService.kt
@@ -1,4 +1,18 @@
 package com.event_app_backend.application
 
-class EventService {
+import com.event_app_backend.domain.model.event.Event
+import com.event_app_backend.domain.repository.EventRepository
+import org.springframework.stereotype.Service
+
+@Service
+class EventService (
+  private val eventRepository: EventRepository
+  ){
+
+  /**
+   * get Events
+   */
+  fun getEvents():List<Event>{
+    return eventRepository.getEvents()
+  }
 }
--- src/main/kotlin/com/event_app_backend/infrastructure/dao/EventDao.kt
+++ src/main/kotlin/com/event_app_backend/infrastructure/dao/EventDao.kt
@@ -1,4 +1,15 @@
 package com.event_app_backend.infrastructure.dao
 
-class EventDao {
+import com.event_app_backend.infrastructure.table.EventTableRecord
+import org.seasar.doma.Dao
+import org.seasar.doma.Select
+import org.seasar.doma.boot.ConfigAutowireable
+
+@ConfigAutowireable
+@Dao
+interface EventDao {
+
+  @Select
+  fun getEvents():List<EventTableRecord>
+
 }
